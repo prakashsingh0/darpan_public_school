6045985153908842ea704bc3eac250ddc99e9d66
Prakash singh <singhprakash545@gmail.com>
2025-09-21 23:39:04 +0530

    fix path

--- server/src/index.js
+++ server/src/index.js
@@ -21,7 +21,7 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes);
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../client/dist")));
 
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
