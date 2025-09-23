e03b80c15200c3aeb8334988c8a858a916ad345a
Prakash singh <singhprakash545@gmail.com>
2025-09-23 17:25:55 +0530

    url update

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? 'http://localhost:8000/api/auth/v1/' : "/api",
+    baseURL: import.meta.env.MODE === "development" ? 'http://localhost:8000/api/auth/v1/' : "/api/auth/v1/",
     withCredentials: true
 })
